b005758b22f520040868440bbb01ed332501b55f
Morgan Quigley <morgan@osrfoundation.org>
2015-07-13 22:39:44 -0700

    generates stm32f7 binfile

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 a free, portable, minimalist RTPS implementation
 
 extremely high awesome factor
+
+To build it, just type "make" and it will spin CMake up a few times to build freertps for a few different systems.
--- systems/stm32_common/cmake/stm32_common.cmake
+++ systems/stm32_common/cmake/stm32_common.cmake
@@ -1,7 +1,7 @@
-set(CMAKE_SYSTEM_NAME Generic)
-set(CMAKE_C_COMPILER arm-none-eabi-gcc)
-set(CMAKE_RANLIB CACHE STRING arm-none-eabi-ranlib)
-set(CMAKE_AR CACHE STRING arm-none-eabi-ar)
+#set(CMAKE_SYSTEM_NAME Generic)
+#set(CMAKE_C_COMPILER arm-none-eabi-gcc)
+#set(CMAKE_RANLIB CACHE STRING arm-none-eabi-ranlib)
+#set(CMAKE_AR CACHE STRING arm-none-eabi-ar)
 #include(CMakeForceCompiler)
 #CMAKE_FORCE_C_COMPILER(arm-none-eabi-gcc GNU)
 set(FPU_FLAGS "-mfloat-abi=hard -mfpu=fpv4-sp-d16")
@@ -10,3 +10,13 @@ set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -ffunction-sections -fdata-sections")
 set(CMAKE_SHARED_LIBRARY_LINK_C_FLAGS "")
 set(CMAKE_EXE_LINKER_FLAGS "-lc -lgcc -mthumb -static -Wl,--no-gc-sections -Wl,-Map=${CMAKE_CURRENT_BINARY_DIR}/map,--cref")
 set(CMAKE_EXECUTABLE_SUFFIX .elf)
+
+set(make_binfiles ON CACHE BOOL "build binaries from ELFs")
+function(make_bin exe elf bin)
+  #message("hello i will now turn ${elf} into ${bin}")
+  add_custom_command(OUTPUT ${bin}
+                     COMMAND arm-none-eabi-objcopy -O binary ${elf} ${bin}
+                     DEPENDS ${elf}
+                     COMMENT "creating ${bin}")
+  add_custom_target(${exe}_bin ALL DEPENDS ${bin})
+endfunction()
