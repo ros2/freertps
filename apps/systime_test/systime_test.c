6f7ab1cf8b6e7748ffe53291f9043bda6c122c08
Morgan Quigley <morgan@osrfoundation.org>
2015-09-11 20:54:47 -0700

    fix systime for samv/sams

--- apps/systime_test/systime_test.c
+++ apps/systime_test/systime_test.c
@@ -5,21 +5,12 @@
 
 int main(int argc, char **argv)
 {
-  srand(0);
   while (1)
   {
-    for (volatile int i = 0; i < 200000; i++);
-    uint32_t rand_offset = rand() % 10000;
-    for (volatile int i = 0; i < rand_offset; i++);
-    //float f = 1.0e-6 * (float)systime_usecs();
-    //printf("%.6f\r\n", (double)f);
-    systime_usecs();
-    //printf("%4d\r\n", (int)t);
-    /*
-    double f = (double)t * 10.0f;
-    printf("%10d    %f\r\n", (int)t, f);
-    */
+    uint32_t waste_time = 200000 + rand() % 10000;
+    for (volatile int i = 0; i < waste_time; i++);
+    float f = 1.0e-6 * (float)systime_usecs();
+    printf("%.6f\r\n", (double)f);
   }
   return 0;
 }
-
--- systems/samv71_xplained_ultra-metal/systime.c
+++ systems/samv71_xplained_ultra-metal/systime.c
@@ -7,20 +7,24 @@ void systime_init()
   // we'll use the first counter to divide down to microseconds,
   // and chain the second 16-bit counter into the third counter
   // to result in a 32-bit count of microseconds since power-up.
-  PMC->PMC_PCER0 |= (1 << ID_TC0);
+  PMC->PMC_PCER0 |= (1 << ID_TC0) | (1 << ID_TC1) | (1 << ID_TC2);
   // we want to feed TIMER_CLOCK2 = MCK/8 into timer/counter channel 0
   // we're running the system at 288 MHz, so we'll get 288/2/8 = 18 MHz
   // need to use TCCLKS to select TIMER_CLOCK2 as TCLK0
   // need to select TCLK0 as input to counter 0 in TC0XC0S mux
 
   // cascade counters into each other
-  /*
   TC0->TC_BMR =
     TC_BMR_TC0XC0S_TCLK0 |
     TC_BMR_TC1XC1S_TIOA0 |
     TC_BMR_TC2XC2S_TIOA1 |
     TC_BMR_MAXFILT(63);
-    */
+  /*
+  uint32_t tc0 = (uint32_t)&TC0->TC_CHANNEL[0];
+  uint32_t tc1 = (uint32_t)&TC0->TC_CHANNEL[1];
+  uint32_t tc2 = (uint32_t)&TC0->TC_CHANNEL[2];
+  printf("0x%08x  0x%08x  0x%08x\r\n", tc0, tc1, tc2);
+  */
    
   // we need WAVSEL = 10 (without trigger) to have RC reset the counter
   // so we can have microseconds streaming out of the first block
@@ -35,25 +39,42 @@ void systime_init()
   TC0->TC_CHANNEL[0].TC_CCR = TC_CCR_CLKEN | TC_CCR_SWTRG;
   // now, feed the output of block 0 into block 1
   TC0->TC_CHANNEL[1].TC_CMR =
-    //TC_CMR_TCCLKS_XC1   |
-    TC_CMR_TCCLKS_TIMER_CLOCK2   |
+    TC_CMR_TCCLKS_XC1   |
+    //TC_CMR_TCCLKS_TIMER_CLOCK2   |
     TC_CMR_ACPA_SET     |
     TC_CMR_ACPC_CLEAR   |
     TC_CMR_WAVE         |
-    TC_CMR_WAVSEL_UP_RC ;
-  TC0->TC_CHANNEL[1].TC_RC = 65535; 
-  TC0->TC_CHANNEL[1].TC_RA = 31768; // set output high halfway through
+    TC_CMR_WAVSEL_UP    ;
+  TC0->TC_CHANNEL[1].TC_RA = 32768; // set output high halfway through
   TC0->TC_CHANNEL[1].TC_CCR = TC_CCR_CLKEN | TC_CCR_SWTRG;
-  //TC0->TC_BCR = TC_BCR_SYNC;
+
+  TC0->TC_CHANNEL[2].TC_CMR =
+    TC_CMR_TCCLKS_XC2   |
+    //TC_CMR_TCCLKS_TIMER_CLOCK2   |
+    TC_CMR_WAVE         |
+    TC_CMR_WAVSEL_UP_RC ;
+  TC0->TC_CHANNEL[2].TC_CCR = TC_CCR_CLKEN | TC_CCR_SWTRG;
+
+  TC0->TC_BCR = TC_BCR_SYNC;
 }
 
 uint32_t systime_usecs()
 {
-  uint32_t t0 = TC0->TC_CHANNEL[0].TC_CV;
+  __disable_irq();
   uint32_t t1 = TC0->TC_CHANNEL[1].TC_CV;
-  printf("%4d   %8d   %08x  %08x\r\n", (int)t0, (int)t1, 
+  uint32_t t2 = TC0->TC_CHANNEL[2].TC_CV;
+  __enable_irq();
+  // todo: handle pathological case where t1 rolls over in between reads to
+  // t1 and t2. that's extremely unlikely, but it will cause time to jump back
+  // and forth when that does happen, which could lead to badness.
+  uint32_t t = t1 | (t2 << 16);
+  //printf("%d\r\n", (int)t);
+  return t;
+  /*
+  printf("%4d   %8d   %8d  , %08x %08x %08x\r\n",
+      (int)t0, (int)t1, (int)t2,
       (unsigned)TC0->TC_CHANNEL[0].TC_SR,
-      (unsigned)TC0->TC_CHANNEL[1].TC_SR);
-  //return TC0->TC_CHANNEL[0].TC_CV;
-  return 0;
+      (unsigned)TC0->TC_CHANNEL[1].TC_SR,
+      (unsigned)TC0->TC_CHANNEL[2].TC_SR);
+  */
 }
